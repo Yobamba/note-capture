d4e60e421b50e8f99500cd3856b43ced9c0af809
Oluwakemi Richard <52783711+Oluwakemi-Richard@users.noreply.github.com>
2023-11-16 00:19:16 +0000

    working on notes endpoints

--- app.js
+++ app.js
@@ -5,8 +5,22 @@ const mongodb = require("./db/connect");
 const express = require("express");
 const cors = require("cors");
 const port = process.env.PORT || 8080;
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+
 
 const app = express();
+app
+  .use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+  .use(bodyParser.json())
+  .use(cors({ origin: '*' }))
+    .use((req, res, next) => {
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+        next();
+    })
+  .use('/', require('./routes'));
+
 
 mongodb.initDb((err, mongodb) => {
   if (err) {
