49b195de67f6ab98b541d28fd90f961f962fee76
Ednilson <ednilsoncastanheira53@gmail.com>
2023-11-17 14:00:50 +0000

    testing in localhost

--- app.js
+++ app.js
@@ -5,22 +5,56 @@ const mongodb = require("./db/connect");
 const express = require("express");
 const cors = require("cors");
 const port = process.env.PORT || 8080;
-const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('./swagger.json');
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger.json");
 
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+const mongoose = require("mongoose");
+const findOrCreate = require("mongoose-findorcreate");
+const User = require("./user.js");
 
 const app = express();
 app
-  .use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+  .use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument))
   .use(bodyParser.json())
-  .use(cors({ origin: '*' }))
-    .use((req, res, next) => {
-        res.setHeader('Access-Control-Allow-Origin', '*');
-        res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-        next();
-    })
-  .use('/', require('./routes'));
+  .use(cors({ origin: "*" }))
+  .use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    next();
+  })
+  .use("/", require("./routes"));
 
+app.use(
+  session({
+    secret: "Little secret",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Serialize the user to store in the session
+passport.serializeUser((User, done) => {
+  console.log("Serialize user: ", User);
+  done(null, User.id); // Assuming you have an "id" field in your User model
+});
+
+// Deserialize the user from the session
+passport.deserializeUser((id, done) => {
+  console.log("Deserialize user: ", id);
+  User.findById(id)
+    .then((user) => {
+      done(null, user);
+    })
+    .catch((err) => {
+      done(err, null);
+    });
+});
 
 mongodb.initDb((err, mongodb) => {
   if (err) {
