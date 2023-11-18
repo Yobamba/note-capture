455accbb76aa4ff358732da05d8b31d9a0df3923
Ednilson <ednilsoncastanheira53@gmail.com>
2023-11-18 15:09:11 +0000

    protected the trash endpoints

--- routes/trash.js
+++ routes/trash.js
@@ -1,45 +1,78 @@
 const router = require("express").Router();
+const trashController = require("../controllers/trash");
 
-const trashController = require("../controllers/trash", () => {
+// Define the ensureAuthenticated middleware
+const ensureAuthenticated = (req, res, next) => {
+  if (req.isAuthenticated()) {
+    console.log("you're authenticated now!");
+    return next(); // User is authenticated, proceed to the next middleware
+  }
+
+  res.status(401).json({
+    message:
+      "Authentication required. In the browser url remove doc and replace it with start_page/sign-in",
+  });
+};
+
+router.get(
+  "/:noteId",
+  ensureAuthenticated,
+  trashController.getTrashById,
+  () => {
+    /**
+     * #swagger.tags = ["Trash"]
+     */
+  }
+);
+
+router.get("/", ensureAuthenticated, trashController.getAllTrash, () => {
   /**
    * #swagger.tags = ["Trash"]
    */
 });
 
-router.get("/:noteId", trashController.getTrashById, () => {
-  /**
-   * #swagger.tags = ["Trash"]
-   */
-});
+router.post(
+  "/:noteId/trash",
+  ensureAuthenticated,
+  trashController.addToTrash,
+  () => {
+    /**
+     * #swagger.tags = ["Trash"]
+     */
+  }
+);
 
-router.get("/", trashController.getAllTrash, () => {
-  /**
-   * #swagger.tags = ["Trash"]
-   */
-});
+router.delete(
+  "/deleteAllTrash",
+  ensureAuthenticated,
+  trashController.deleteAllTrash,
+  () => {
+    /**
+     * #swagger.tags = ["Trash"]
+     */
+  }
+);
 
-router.post("/:noteId/trash", trashController.addToTrash, () => {
-  /**
-   * #swagger.tags = ["Trash"]
-   */
-});
+router.put(
+  "/:noteId/restore",
+  ensureAuthenticated,
+  trashController.restoreTrash,
+  () => {
+    /**
+     * #swagger.tags = ["Trash"]
+     */
+  }
+);
 
-router.delete("/deleteAllTrash", trashController.deleteAllTrash, () => {
-  /**
-   * #swagger.tags = ["Trash"]
-   */
-});
-
-router.put("/:noteId/restore", trashController.restoreTrash, () => {
-  /**
-   * #swagger.tags = ["Trash"]
-   */
-});
-
-router.delete("/:noteId", trashController.deleteSingleTrash, () => {
-  /**
-   * #swagger.tags = ["Trash"]
-   */
-});
+router.delete(
+  "/:noteId",
+  ensureAuthenticated,
+  trashController.deleteSingleTrash,
+  () => {
+    /**
+     * #swagger.tags = ["Trash"]
+     */
+  }
+);
 
 module.exports = router;
