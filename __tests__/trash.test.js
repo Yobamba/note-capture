ff71a2dda40897e3ac4310bedb59f43fcf872d37
Oluwakemi Richard <52783711+Oluwakemi-Richard@users.noreply.github.com>
2023-12-05 22:04:38 +0000

    error handling and unit tests done

---
+++ __tests__/trash.test.js
@@ -0,0 +1,103 @@
+const mongodb = require("../db/connect");
+const server = require("../server.js");
+const ObjectId = require("mongodb").ObjectId;
+const dotenv = require("dotenv");
+dotenv.config();
+const MongoClient = require("mongodb").MongoClient;
+const request = require("supertest");
+const express = require("express");
+const app2 = express();
+
+// Mock authenticated user for testing
+const authenticatedUser = {
+  googleId: 'x',
+  
+};
+
+// Mock passport.authenticate middleware
+const authenticateMock = (req, res, next) => {
+  req.user = authenticatedUser;
+  next();
+};
+
+describe("Testing the trash endpoints", function () {
+  let connection;
+  let db;
+  let insertedNoteId;
+
+  beforeAll(async () => {
+    connection = await MongoClient.connect(process.env.MONGODB_URI);
+    db = await connection.db(globalThis.__MONGO_DB_NAME__);
+  });
+
+  afterAll(async () => {
+    await connection.close();
+    await server.close();
+  });
+
+  describe("Insert a mock trash to the collection", function () {
+    it("should respond to a POST request", async () => {
+      try {
+        const newObjectId = new ObjectId();
+
+        const mockTrash = {
+          _id: newObjectId,
+          title: "Test Note",
+          note: "This is a test note",
+          noteTags: ["TestTag"],
+          user: "testUser",
+          googleId: "testGoogleId",
+          pinStatus: false,
+          attachments: [],
+        };
+
+        const trash = db.collection("trash");
+        await trash.insertOne(mockTrash);
+        insertedNoteId = newObjectId;
+
+        const trashedNote = await trash.findOne({ _id: newObjectId });
+        expect(trashedNote).toEqual(mockTrash);
+      } catch (error) {
+        console.error(error);
+        throw error;
+      }
+    });
+
+    afterEach(async () => {
+      // Clean up: delete the inserted note after each test
+      if (insertedNoteId) {
+        const notes = db.collection("notes");
+        await notes.deleteOne({ _id: insertedNoteId });
+      }
+    });
+  });
+
+  describe('Testing the trash endpoints', () => {
+    // Applying middleware to the route
+    app2.get('/trash', authenticateMock, (req, res) => {
+      res.status(200).json({ message: 'Mocked response' });
+    });
+    it('should return a 200', async () => {
+        const response = await request(app2)
+            .get('/trash')
+            .expect(200);
+        expect(response.status).toBe(200);
+    });
+  });
+  describe("Get a specific trash from the collection", function () {
+    // Apply the middleware to the other route
+    app2.get('/trash/6569dc75789506885c087ec', authenticateMock, (req, res) => {
+      res.status(200).json({ message: 'Mocked response' });
+    });
+
+    it("should return a 200", async () => {
+      try {
+        const response = await request(app2).get('/trash/6569dc75789506885c087ec');
+        expect(response.status).toBe(200);
+      } catch (error) {
+        console.error(error);
+        throw error;
+      }
+    });
+  });
+});
--- controllers/notes.js
+++ controllers/notes.js
@@ -4,13 +4,18 @@ const tagsController = require("../controllers/tags");
 const addToTrash = require('./trash').addToTrash;
 
 const getNotes = async (req, res) => {
-  const googleId = req.user.googleId;
-  const db = await mongodb.getDb();
-  const result = await db.db().collection("notes").find({ googleId }); // only get notes by the authenticated user
-  result.toArray().then((lists) => {
+  try{
+    const googleId = req.user.googleId;
+    const db = await mongodb.getDb();
+    const result = await db.db().collection("notes").find({ googleId }); // only get notes by the authenticated user
+    result.toArray().then((lists) => {
     res.setHeader("Content-Type", "application/json");
     res.status(200).json(lists);
   });
+  }catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Sorry, an error occurred while fetching notes." });
+  }
 };
 
 const getNote = async (req, res) => {
@@ -29,12 +34,13 @@ const getNote = async (req, res) => {
       return;
     }
   } catch {
-    res.status(500).json({ error: "Sorry, an error occurred while moving the note to trash." });
+    res.status(500).json({ error: "Sorry, an error occurred while getting note." });
   }
   
 };
 
 const createNote = async (req, res) => {
+  try{
   const db = await mongodb.getDb();
 
   // noteTags should be a list of tags
@@ -68,9 +74,14 @@ const createNote = async (req, res) => {
       .status(500)
       .json(response.error || "Sorry, an error occurred while creating note.");
   }
+}catch (error) {
+  console.error(error);
+  res.status(500).json({ error: "Sorry, an error occurred while creating notes." });
+}
 };
 
 const createTagIfNotExists = async (db, tagName) => {
+  try{
   const existingTag = await db
     .db()
     .collection("tags")
@@ -80,31 +91,40 @@ const createTagIfNotExists = async (db, tagName) => {
     const newTag = { name: tagName };
     await db.db().collection("tags").insertOne(newTag);
   }
+}catch (error) {
+  console.error(error);
+  res.status(500).json({ error: "Sorry, an error occurred." });
+}
 };
 
 
 const updateNote = async (req, res) => {
-  const db = await mongodb.getDb();
-  const noteId = new ObjectId(req.params.id);
-  const note = {
-    title: req.body.title,
-    note: req.body.note,
-    noteTags: req.body.noteTags,
-    user: req.body.user,
-    pinStatus: req.body.pinStatus,
-    attatchments: req.body.attatchments,
-  };
-  const response = await db
-    .db()
-    .collection("notes")
-    .replaceOne({ _id: noteId }, note);
-  console.log(response);
-  if (response.modifiedCount > 0) {
-    res.status(204).send();
-  } else {
-    res
-      .status(500)
-      .json(response.error || "Sorry, an error occured while updating note.");
+  try{
+    const db = await mongodb.getDb();
+    const noteId = new ObjectId(req.params.id);
+    const note = {
+      title: req.body.title,
+      note: req.body.note,
+      noteTags: req.body.noteTags,
+      user: req.body.user,
+      pinStatus: req.body.pinStatus,
+      attatchments: req.body.attatchments,
+    };
+    const response = await db
+      .db()
+      .collection("notes")
+      .replaceOne({ _id: noteId }, note);
+    console.log(response);
+    if (response.modifiedCount > 0) {
+      res.status(204).send();
+    } else {
+      res
+        .status(500)
+        .json(response.error || "Sorry, an error occured while updating note.");
+    }
+  }catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Sorry, an error occurred while updating notes." });
   }
 };
 
@@ -144,64 +164,74 @@ const findByTag = async (req, res) => {
 };
 
 const addTagToNote = async (req, res) => {
-  const noteId = new ObjectId(req.params.noteId);
-  const newTag = req.body.newTag;
-  console.log(newTag); // 
-//
-  if (!newTag) {
-    res.status(400).json({ error: "New tag is required" });
-    return;
-  }
+  try{
+    const noteId = new ObjectId(req.params.noteId);
+    const newTag = req.body.newTag;
+    console.log(newTag); // 
+  //
+    if (!newTag) {
+      res.status(400).json({ error: "New tag is required" });
+      return;
+    }
 
-  const db = await mongodb.getDb();
-  const existingNote = await db
-    .db()
-    .collection("notes")
-    .findOne({ _id: noteId });
+    const db = await mongodb.getDb();
+    const existingNote = await db
+      .db()
+      .collection("notes")
+      .findOne({ _id: noteId });
 
-  if (!existingNote) {
-    res.status(404).json({ error: `Note with ID ${noteId} not found` });
-    return;
-  }
+    if (!existingNote) {
+      res.status(404).json({ error: `Note with ID ${noteId} not found` });
+      return;
+    }
 
-  await createTagIfNotExists(db, newTag);
+    await createTagIfNotExists(db, newTag);
 
-  const updatedNoteTags = Array.isArray(existingNote.noteTags)
-    ? existingNote.noteTags
-    : [existingNote.noteTags];
-  updatedNoteTags.push(newTag);
+    const updatedNoteTags = Array.isArray(existingNote.noteTags)
+      ? existingNote.noteTags
+      : [existingNote.noteTags];
+    updatedNoteTags.push(newTag);
 
-  const response = await db
-    .db()
-    .collection("notes")
-    .updateOne({ _id: noteId }, { $set: { noteTags: updatedNoteTags } });
+    const response = await db
+      .db()
+      .collection("notes")
+      .updateOne({ _id: noteId }, { $set: { noteTags: updatedNoteTags } });
 
-  if (response.modifiedCount > 0) {
-    res
-      .status(200)
-      .json({ message: `Tag '${newTag}' added to note with ID ${noteId}` });
-  } else {
-    res
-      .status(500)
-      .json(response.error || "Sorry, an error occurred while adding the tag.");
-  }
+    if (response.modifiedCount > 0) {
+      res
+        .status(200)
+        .json({ message: `Tag '${newTag}' added to note with ID ${noteId}` });
+    } else {
+      res
+        .status(500)
+        .json(response.error || "Sorry, an error occurred while adding the tag.");
+    }
+}catch (error) {
+  console.error(error);
+  res.status(500).json({ error: "Sorry, an error occurred while adding the tag." });
+}
 };
 const createUser = async (req, res) => {
-  const user = {
-    email: req.body.email,
-    password: req.body.password,
-    username: req.body.userName,
-  };
-  const db = await mongodb.getDb();
-  const response = await db.db().collection("users").insertOne(user);
-  if (response.acknowledged) {
-    res.redirect("https://note-capture.onrender.com/start_page/login");
-  } else {
-    res
-      .status(500)
-      .json(response.error || "Some error occurred while creating the user.");
-    console.log("Erro!");
-  }
+  try{
+    const user = {
+      email: req.body.email,
+      password: req.body.password,
+      username: req.body.userName,
+    };
+    const db = await mongodb.getDb();
+    const response = await db.db().collection("users").insertOne(user);
+    if (response.acknowledged) {
+      res.redirect("https://note-capture.onrender.com/start_page/login");
+    } else {
+      res
+        .status(500)
+        .json(response.error || "Some error occurred while creating the user.");
+      console.log("Erro!");
+    }
+}catch (error) {
+  console.error(error);
+  res.status(500).json({ error: "Sorry, an error occurred while creating user." });
+}
 };
 
 module.exports = {
